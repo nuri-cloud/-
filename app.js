2aecdc12dd33441f2493a2e39b8f1ebdf26f3881
nuri-cloud <nurzadatobokelova@gmail.com>
2024-11-20 00:37:14 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// function declaration(a, b) {
+//     return a + + b;
+// }
+// console.log(declaration( 5, '10'));
+
+
+
+//функция менен
+
+// function colculator (a, b) {
+//     console.log(`Cложение: ${a} + ${b} = ${a + b}`);
+//     console.log(`Вычитание: ${a} - ${b} = ${a - b}`);
+//     console.log(`Умножение: ${a} * ${b} = ${a * b}`);
+//     console.log(`Деление: ${a} / ${b} = ${a / b}`);
+//   }
+//   colculator(12, 12);
+
+
+
+//switch case менен
+
+// const num1 = prompt('сан жаз 1');
+// num1 = Number(num1);
+
+// const num2 = prompt('сан жаз 2');
+// num2 = Number(num2);
+
+
+// const operator = prompt('оператор (/,+,*,-)');
+
+
+// switch (operator) {
+//     case '+':
+//         alert (num1 + num2);
+//         break;
+//     case '-':
+//         alert (num1 - num2);
+//         break;
+//     case '/':
+//         alert (num1 / num2);
+//         break;
+//     case '*':
+//         alert (num1 * num2);
+        
+     
+// }
